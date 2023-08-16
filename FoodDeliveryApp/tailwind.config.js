82f60952bf3d8fdb82a0d20b2a6c1dcb6ae3f8c7
Hendrix1995 <hgud55@naver.com>
2023-08-16 15:40:20 +0900

    feat: nativewind 세팅

--- FoodDeliveryApp/tailwind.config.js
+++ FoodDeliveryApp/tailwind.config.js
@@ -7,23 +7,27 @@ const COLORS = {
   },
 };
 
-export const theme = {
-  extend: {
-    colors: COLORS,
-    fontSize: {
-      8: '8px',
-      10: '10px',
-      11: '11px',
-      12: '12px',
-      13: '13px',
-      14: '14px',
-      16: '16px',
-      18: '18px',
-      20: '20px',
-      22: '22px',
-      24: '24px',
-      26: '26px',
-      28: '28px',
+module.exports = {
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
+  theme: {
+    extend: {
+      colors: COLORS,
+      fontSize: {
+        8: '8px',
+        10: '10px',
+        11: '11px',
+        12: '12px',
+        13: '13px',
+        14: '14px',
+        16: '16px',
+        18: '18px',
+        20: '20px',
+        22: '22px',
+        24: '24px',
+        26: '26px',
+        28: '28px',
+      },
     },
   },
+  plugins: [],
 };
